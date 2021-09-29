b7fa0de95f4c58de2f14f00cac3aad993f2162ee
lkon0010 <lkon0010@uni.sydney.edu.au>
2021-09-29 10:55:18 +0800

    Initialise WIFI manager for WIFI operations

--- app/src/main/java/com/project/swapper/MainActivity.java
+++ app/src/main/java/com/project/swapper/MainActivity.java
@@ -2,13 +2,31 @@ package com.project.swapper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 
+import java.util.List;
+
+/**
+ * The main screen activity when the user opens this application.
+ */
 public class MainActivity extends AppCompatActivity {
+    // WIFI Manager
+    private WifiManager mWifiManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Uses "activity_main.xml" as the layout
         setContentView(R.layout.activity_main);
+
+        // Attains the wifi manager
+        mWifiManager = (WifiManager)
+                this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+//        List<ScanResult> results = mWifiManager.getScanResults();
     }
 }
