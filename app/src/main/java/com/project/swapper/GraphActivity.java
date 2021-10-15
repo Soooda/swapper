464958c3855a2b59704ea1797cd2a0ecdf448396
Jialin'G <50652172+jiajy0427@users.noreply.github.com>
2021-10-15 21:37:06 +1100

    main activity (#19)
    * the first version of the main activity, with some issues
    
    * the first version of the main activity, with some issues
    
    * the first version of the main activity, with some issues
    
    * new gradle and boolean variables
    
    * Delete deploymentTargetDropDown.xml
    
    Co-authored-by: Hilbert Kong <kongliangxm@foxmail.com>

--- app/src/main/java/com/project/swapper/GraphActivity.java
+++ app/src/main/java/com/project/swapper/GraphActivity.java
@@ -16,6 +16,7 @@ import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class GraphActivity extends AppCompatActivity {
@@ -102,4 +103,6 @@ public class GraphActivity extends AppCompatActivity {
     public void stopSignal(View view) {
         mHandler.removeCallbacks(mTimer);
     }
+
+
 }
