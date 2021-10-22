c9fd4357feb74e33888680dee077c2adfe1c5197
Niko He <zhichaohh@163.com>
2021-10-22 18:31:29 +0800

    update setting activity

--- app/src/main/java/com/project/swapper/SettingActivity.java
+++ app/src/main/java/com/project/swapper/SettingActivity.java
@@ -1,5 +1,14 @@
 package com.project.swapper;
 
-public class SettingActivity {
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
 
 }
