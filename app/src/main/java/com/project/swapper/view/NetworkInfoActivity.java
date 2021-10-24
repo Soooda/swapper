1e3683cd859f11e5e2dcbc91d52fdc26456fa172
lkon0010 <lkon0010@uni.sydney.edu.au>
2021-10-24 23:54:35 +0800

    NetworkInfoActivity Layout

---
+++ app/src/main/java/com/project/swapper/view/NetworkInfoActivity.java
@@ -0,0 +1,13 @@
+package com.project.swapper.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class NetworkInfoActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
