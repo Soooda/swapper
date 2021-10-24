b15f5db986adc635bf81ab34c8ae7e1dba542fd5
lkon0010 <lkon0010@uni.sydney.edu.au>
2021-10-24 23:54:19 +0800

    MainActivity Layout

--- app/src/main/java/com/project/swapper/MainActivity.java
+++ app/src/main/java/com/project/swapper/MainActivity.java
@@ -8,5 +8,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Uses "activity_main.xml" as the layout
+        setContentView(R.layout.activity_main);
     }
 }
