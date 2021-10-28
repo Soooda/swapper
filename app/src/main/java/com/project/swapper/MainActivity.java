a3bfc94bbfb3bd845c6bde2246030dde4637b643
Farros Mufid <farrosmufid.ofid@gmail.com>
2021-10-28 18:07:36 +0700

    Update Main

--- app/src/main/java/com/project/swapper/MainActivity.java
+++ app/src/main/java/com/project/swapper/MainActivity.java
@@ -9,22 +9,10 @@ import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-<<<<<<< HEAD
 import androidx.appcompat.widget.SwitchCompat;
-=======
-import com.project.swapper.security.Network;
-import com.project.swapper.security.NetworkListAdapter;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
->>>>>>> main
 
 import com.project.swapper.view.NetworkInfoActivity;
 
-<<<<<<< HEAD
 public class MainActivity extends Activity {
     private Model model;
 
@@ -32,23 +20,12 @@ public class MainActivity extends Activity {
     private TextView serviceStatus;
     private SwitchCompat batterySwitch;
     private ImageButton wifiButton;
-=======
-    Network network1 = new Network("network1", true, true, 0);
-    Network network2 = new Network("network2", false, false, 0);
-    Network network3 = new Network("network3", false, true, 0);
-    Network network4 = new Network("network4", false, true, 0);
-    Network network5 = new Network("network5", false, false, 0);
-    Network network6 = new Network("network6", false, true, 0);
-    Network network7 = new Network("network7", false, true, 0);
-    Network network8 = new Network("network8", false, true, 0);
->>>>>>> main
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Uses "activity_main.xml" as the layout
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
 
         // Model Init
         Model.createInstance(this);
@@ -59,23 +36,6 @@ public class MainActivity extends Activity {
         serviceStatus = findViewById(R.id.service_status);
         batterySwitch = findViewById(R.id.battery);
         wifiButton = findViewById(R.id.wifi);
-=======
-
-        ImageButton btnGraphView = findViewById(R.id.graph_view);
-
-        ListView resultsListView = (ListView) findViewById(R.id.results_listView);
-        ArrayList<Network> NetworkList = new ArrayList<>();
-        NetworkList.add(network1);
-        NetworkList.add(network2);
-        NetworkList.add(network3);
-        NetworkList.add(network4);
-        NetworkList.add(network5);
-        NetworkList.add(network6);
-        NetworkList.add(network7);
-        NetworkList.add(network8);
-        NetworkListAdapter adapter = new NetworkListAdapter(this, R.layout.list_item, NetworkList);
-        resultsListView.setAdapter(adapter);
->>>>>>> main
 
         // If the service is already running
         if (model.isServiceRunning()) {
@@ -120,8 +80,4 @@ public class MainActivity extends Activity {
             }
         });
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> main
 }
